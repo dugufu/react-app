fb3165c678cfbf4714834c15a95d257697860f35
dugufu <cchunren1997@gmail.com>
2023-04-03 22:43:30 +0800

    2023/04/03

--- src/App.js
+++ src/App.js
@@ -2,134 +2,25 @@ import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
 
-class Square extends React.Component {
-  render() {
+function formatName(user) {
+  return user.firstName + " " + user.lastname;
+}
+
+function greeting(user) {
+  if (user) {
     return (
-      <button>
-        {
-          {
-            /* To do here */
-          }
-        }
-      </button>
+      <h1>
+        ("Greeting, ", {user.firstName} {user.lastname})
+      </h1>
     );
+  } else {
+    return <h1>("Greeting, stranger")</h1>;
   }
 }
 
-class Board extends React.Component {
-  renderSquare(i) {
-    return <Square />;
-  }
+const user = {
+  firstName: "",
+  lastname: "",
+};
 
-  render() {
-    const status = "Next player: X";
-
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(8)}
-          {this.renderSquare(9)}
-        </div>
-      </div>
-    );
-  }
-}
-
-// export default class extends React.Component {
-//   render() {
-//     return (
-//       <div className="game">
-//         <div className="game-board">
-//           <Board />
-//         </div>
-//         <div className="game-info">
-//           <div>
-//             {
-//               {
-//                 /* status */
-//               }
-//             }
-//           </div>
-//           <div>
-//             {
-//               {
-//                 /* TODO */
-//               }
-//             }
-//           </div>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// const user = {
-//   name: "dota1212sdasdas2",
-//   url: "https://pbs.twimg.com/profile_images/1478893871199186945/1mA6tezL_400x400.jpg",
-//   imageSize: 90,
-// };
-
-// export default function gameLogo() {
-//   return (
-//     <div>
-//       <h1>{user.name}</h1>
-//       <img
-//         className="avatar"
-//         src={user.url}
-//         alt={"Game Logo of " + user.name}
-//         style={{ with: user.imageSize, height: user.imageSize }}
-//       />
-//     </div>
-//   );
-// }
-
-//button
-/* function MyButton() {
-  return (
-    <button>
-      I'm a button
-    </button>
-  );
-}
-
-export default function MyApp() {
-  return (
-    <div>
-      <h1>Welcome to my app</h1>
-      <MyButton />
-    </div>
-  );
-} */
+const element = <h1>Hello, {formatName(user)}!</h1>; //直接把object丢进来
--- src/index.js
+++ src/index.js
@@ -3,61 +3,107 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
-class Square extends React.Component {
-  render() {
-    return <button className="square">{/* To do here */}</button>;
-  }
+function Welcome(props) {
+  return <h1>This is {props.name}</h1>;
 }
 
-class Board extends React.Component {
-  renderSquare(i) {
-    return <Square />;
-  }
-
-  render() {
-    const status = "Next player: X";
-
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(8)}
-          {this.renderSquare(9)}
-        </div>
-      </div>
-    );
-  }
+function Game(props) {
+  return <h1>This is the best game ever : {props.bestGameName}</h1>;
 }
 
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <div>{/* TODO */}</div>
-        </div>
-      </div>
-    );
-  }
+const element = (
+  <>
+    <Welcome name="Sarah" />
+    <Welcome name="chow" />
+    <Welcome name="Xian" />
+    <Welcome name="Leh" />
+
+    <Game bestGameName="Dota2" />
+  </>
+);
+
+function formatDate(date) {
+  return date.toLocaleDateString();
 }
+
+function Avatar(props) {
+  return (
+    <img className="Avatar" src={props.avatar.url} alt={props.avatar.name} />
+  );
+}
+
+function UserInfo(props) {
+  return (
+    <div className="UserInfo">
+      <Avatar avatar={props.avatarLink} />
+      <div className="UserInfo-name">{props.user.name}</div>
+    </div>
+  );
+}
+
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <UserInfo user={props.author} avatarLink={props.avatar} />
+      <div className="Comment-text">{props.text}</div>
+      <div className="Comment-date">{formatDate(props.date)}</div>
+    </div>
+  );
+}
+
+const comment = {
+  date: new Date(),
+  text: "I hope you enjoy learning React!",
+  avatar: {
+    url: "https://scontent.fmkz1-1.fna.fbcdn.net/v/t1.6435-9/51132445_1015504495326196_3440377681516953600_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=e3f864&_nc_ohc=PneJcSMx9x0AX-1ykRg&_nc_oc=AQnKoIDWNe0PYIL2UBSQMnOzweHhuEdneJDZVanUCQwYb9OjigNHawVpAxCqqiyWbRlsM2w9K340uj3REpqDSESI&_nc_ht=scontent.fmkz1-1.fna&oh=00_AfAMXoucr6e5yydvDlEnZEUD6zvPTT1Rv_RlWkQAXOkBig&oe=64526067",
+    avatarName: "residentEvil",
+  },
+  author: {
+    name: "Chow Kitty",
+  },
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Game />);
+root.render(
+  <Comment
+    date={comment.date}
+    author={comment.author}
+    text={comment.text}
+    avatar={comment.avatar}
+  />
+);
+
+//define
+// const name = "chow";
+
+// const element2 = <h1>Hello, {name}</h1>;
+
+// function tick() {
+//   const element = (
+//     <div>
+//       <h1>Hello world</h1>
+//       <h2>It is {new Date().toLocaleTimeString()} . </h2>
+//     </div>
+//   );
+//   root.render(element);
+// }
+
+// setInterval(tick, 1000);
+
+//Embedding Expressions in JXS
+//const element3 = <h1>Hello, {formatName(user)}!</h1>; //直接把object丢进来
+
+// const user = {
+//   firstName: "Harper",
+//   lastName: "Perez",
+// };
+
+// function formatName(user) {
+//   return user.firstName + " " + user.lastName;
+// }
+
+//const element4 = <h1>Hello, {formatName(user)}!</h1>;
+
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(
 //   <React.StrictMode>
